bc90c628376fc389a5bc8d743b63df7d4507e480
Cem Vardar <cvardar@gmail.com>
2013-03-01 17:54:42 -0700

    removing passwords from code

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from pymongo import MongoClient
 
 
 def get_names_collection():
-    mongodb_uri = "mongodb://heroku_hello:HerokuHello75@ds053937.mongolab.com:53937/cem_heroku_hello"
+    userid = os.environ['mongolab_userid']
+    password = os.environ['mongolab_password']
+    mongodb_uri = "mongodb://"+userid+":"+ password+ "@ds053937.mongolab.com:53937/cem_heroku_hello"
     db_name = 'cem_heroku_hello'
     try:
         connection = pymongo.Connection(mongodb_uri)
